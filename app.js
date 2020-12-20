328eb23ddd9189458b000bbd14acebaa7eac67ef
Genis Bardales <genis96@yahoo.com>
2020-12-19 20:37:47 -0500

    linked to html selectors and found a way to get the correct dates in JS

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // 78143e709d4093268bfac14691b1a091 - API Key
+let apiKey = '78143e709d4093268bfac14691b1a091';
+let searchInput = document.querySelector('.searchInput');
+let searchBtn = document.querySelector('.searchBtn');
+//get days/days/year
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+let month = String(today.getMonth() + 1).padStart(2, '0');
+let year = today.getFullYear();
+var today = mm + '/' + dd + '/' + yyyy;
