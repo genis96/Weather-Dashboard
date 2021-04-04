9bda4ea6ef0a2902aa758fa8f2319b2926922b72
Genis Bardales <genis96@yahoo.com>
2021-04-04 17:31:02 -0400

    fixing code and working on functions

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 let apiKey = '78143e709d4093268bfac14691b1a091';
-let searchInput = document.querySelector('.searchInput');
-let searchBtn = document.querySelector('.searchBtn');
+let searchInput = $('.searchInput');
+let searchBtn = $('.searchBtn');
+//search 
+let cityName = $('.cityName');
+let currDate = $('.currDate');
+let weatherIcon = $('.weatherIcon');
+let itemHistory = $('.itemHistory');
+// results
+let temp = $('.temp');
+let humidity = $('.humidity');
+let windSpeed = $('.windSpeed');
+let uvIndex = $('.uvIndex');
+let cardRow = $('.card-row');
+
 //get days/days/year
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
-let month = String(today.getMonth() + 1).padStart(2, '0');
-let year = today.getFullYear();
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
 let today = mm + '/' + dd + '/' + yyyy;
-//rest of selectors
-let itemHistory = document.getElementsByClassName('itemHistory');
-let cityName = document.getElementsByClassName('cityName');
-let currDate = document.getElementsByClassName('currDate');
-let weatherIcon = document.getElementsByClassName('weatherIcon');
-let temp = document.getElementsByClassName('temp');
-let humidity = document.getElementsByClassName('humidity');
-let windSpeed = document.getElementsByClassName('windSpeed');
-let uvIndex = document.getElementsByClassName('uvIndex');
-let cardRow = document.getElementsByClassName('card-row');
 
 //create functions 
 //if statement for localStorage where I have to parse the data
